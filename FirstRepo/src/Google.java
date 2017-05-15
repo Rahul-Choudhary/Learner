59e9790020d0131425d61abb8abff3612c881091
Rahul <rahul.choudhary@crestechglobal.com>
2017-05-15 11:31:32 +0530

    First Commit

---
+++ FirstRepo/src/Google.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Google {
+public static void main(String...s)
+{
+WebDriver driver = new FirefoxDriver();
+driver.get("http://www.google.com");
+
+}
+}
